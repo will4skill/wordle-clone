10b74f017da77b528a5ea6309dcf39693cc8b07c
Tim Williams <timwilliams@Tims-MacBook-Pro.local>
2022-06-15 23:22:47 -0400

    Add support for keyboard input and different lines

--- src/Game.js
+++ src/Game.js
@@ -1,26 +1,65 @@
+// Add and remove an event listener for key presses:
+// Add keyboard event listener: https://stackoverflow.com/questions/64434545/react-keydown-event-listener-is-being-called-multiple-times
+// Only look for lowercase letters
+
 import logo from './logo.svg';
 import './Game.css';
 import WordRow from './components/WordRow';
 import Keyboard from './components/Keyboard';
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 function Game() {
-  const [topWord, setTopWord] = useState("");
+  const [wordGrid, setWordGrid] = useState(["", "", "", "", "", ""]);
+  const [currRow, setCurrRow] = useState(0);
+
+  const handleKeyboardInput = (e) => {
+    handleKeyPress(e.key,);
+  };
+
   const handleKeyPress = (key, e) => {
     console.log("KEYPRESS: " + key)
-    if (topWord.length < 5)
-      setTopWord(topWord + key);
+    const wordGridCopy = [...wordGrid];
+    const keyPressed = key;
+
+    if ((keyPressed === "enter" || keyPressed === "Enter") && currRow < 5) {
+      if (wordGrid[currRow].length < 5)
+        alert("Word Too Short");
+      else {
+        setCurrRow(currRow + 1);
+      }
+      return;
+    }
+
+    const currWord = wordGridCopy[currRow];
+
+    if ((keyPressed === "del" || keyPressed === "Backspace") && keyPressed.length) {
+      wordGridCopy[currRow] = currWord.substring(0, currWord.length - 1)
+      setWordGrid(wordGridCopy);
+      return;
+    }
+
+
+
+    if (currWord.length < 5 && /^([a-z]){1}$/.test(keyPressed)) {
+      wordGridCopy[currRow] += keyPressed;
+      setWordGrid(wordGridCopy);
+    }
   };
+
+  useEffect(() => {
+    document.addEventListener("keydown", handleKeyboardInput);
+    return () => document.removeEventListener("keydown", handleKeyboardInput);
+  });
 
   return (
     <div className="App">
       <header className="App-header">
-        <WordRow word={topWord} />
-        <WordRow word={topWord} />
-        <WordRow word={topWord} />
-        <WordRow word={topWord} />
-        <WordRow word={topWord} />
-        <WordRow word={topWord} />
+        <WordRow word={wordGrid[0]} />
+        <WordRow word={wordGrid[1]} />
+        <WordRow word={wordGrid[2]} />
+        <WordRow word={wordGrid[3]} />
+        <WordRow word={wordGrid[4]} />
+        <WordRow word={wordGrid[5]} />
         <Keyboard handleKeyPress={handleKeyPress} />
       </header>
     </div>
--- src/components/Keyboard.js
+++ src/components/Keyboard.js
@@ -3,7 +3,7 @@ import Key from "./Key";
 const Keyboard = ({ handleKeyPress }) => {
     const topRow = ['q', 'u', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'];
     const middleRow = ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'];
-    const bottomRow = ['enter', 'z', 'x', 'c', 'v', 'b', 'n', 'm', 'del'];
+    const bottomRow = ['z', 'x', 'c', 'v', 'b', 'n', 'm'];
 
     return (
         <>
@@ -18,12 +18,11 @@ const Keyboard = ({ handleKeyPress }) => {
                 }
             </div>
             <div style={{ display: "flex", flexDirection: 'row', justifyContent: "center" }}>
-                {
-                    bottomRow.map((letter, index) => {
-                        const wide = (index === 0 || index === bottomRow.length - 1) ? true : false;
-                        return <Key wide={wide} letter={letter} key={index} handleClick={handleKeyPress} />
-                    })
+                <Key wide letter="enter" handleClick={handleKeyPress} />
+                {bottomRow.map((letter, index) =>
+                    <Key letter={letter} key={index} handleClick={handleKeyPress} />)
                 }
+                <Key wide letter="del" handleClick={handleKeyPress} />
             </div>
         </>
     );
