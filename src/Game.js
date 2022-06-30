bb1caded2952384560ac2e19bf873842a0092c66
Tim Williams <timwilliams@Tims-MacBook-Pro.local>
2022-06-29 23:51:46 -0400

    Add modal and timer logic

--- src/Game.js
+++ src/Game.js
@@ -6,6 +6,11 @@
 // Bounce Animation: https://css-tricks.com/snippets/css/keyframe-animation-syntax/
 // Staggering Animations: https://css-tricks.com/different-approaches-for-creating-a-staggered-animation/
 // Add delay: https://stackoverflow.com/questions/42089548/how-to-add-delay-in-reactjs
+// How to make a JS Timer: https://stackoverflow.com/questions/29971898/how-to-create-an-accurate-timer-in-javascript
+// How to clear Intervals and Timeouts with Hooks:
+// https://stackoverflow.com/questions/53090432/react-hooks-right-way-to-clear-timeouts-and-intervals
+// Clear Interval: https://developer.mozilla.org/en-US/docs/Web/API/clearInterval
+// Get current time: https://stackoverflow.com/questions/10599148/how-do-i-get-the-current-time-only-in-javascript
 
 import './Game.css';
 import WordRow from './components/WordRow';
@@ -13,6 +18,10 @@ import Keyboard from './components/Keyboard';
 import React, { useState, useEffect } from 'react';
 import guesses from './guesses';
 import answers from './answers';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
+import Table from 'react-bootstrap/Table';
+import Alert from 'react-bootstrap/Alert';
 
 function Game() {
   const [wordGrid, setWordGrid] = useState(["", "", "", "", "", ""]);
@@ -20,19 +29,116 @@ function Game() {
   const [targetWord, setTargetWord] = useState("");
   const [usedLetters, setUsedLetters] = useState("");
   const [lastKey, setLastKey] = useState("");
-  const [gameState, setGameState] = useState("");
+  const [gameState, setGameState] = useState("new");
+  const [show, setShow] = useState(true);
 
-  const handleKeyboardInput = (e) => {
-    handleKeyPress(e.key,);
-  };
+  const [secondsElapsed, setSecondsElapsed] = useState(0);
+  const [clockState, setClockState] = useState('new');
+  const [tickState, setTickState] = useState(0);
 
-  const resetGame = () => {
+  const [finalTime, setFinalTime] = useState(null);
+
+  const delay = (mili) => new Promise(res => setTimeout(res, mili));
+
+  const resetTargetWord = () => {
+    const randomIdx = Math.floor(Math.random() * answers.length);
+    const randomTargetWord = answers[randomIdx];
+    setTargetWord(randomTargetWord);
+  }
+
+  // console.log(targetWord);
+
+  const startTheGame = () => {
+    stop();
+    start();
     setWordGrid(["", "", "", "", "", ""]);
     setCurrRow(0);
     setUsedLetters("");
     setLastKey("");
-    setGameState("");
+    setGameState("new");
+    resetTargetWord();
+    setShow(false);
   }
+
+  useEffect(() => {
+    let timer = setInterval(() => {
+      setTickState(tickState => tickState + 10);
+    }, 10);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (clockState === 'start') {
+      if (tickState >= 10) {
+        setSecondsElapsed(secondsElapsed + 1);
+        setTickState(0);
+      }
+    }
+
+    if (clockState === 'pause') {
+      // Do Nothing
+    }
+
+    if (clockState === 'new') {
+      setSecondsElapsed(0);
+    }
+  }, [tickState, clockState]);
+
+  const formatTimeString = time => {
+    // Note: time is coming in in hundreths of a second
+
+    // const hours = Math.floor(time/3600)
+    // time %= 3600;
+    const minutes = Math.floor(time / 6000);
+    time %= 6000;
+    const seconds = Math.floor(time / 100);
+    time %= 100;
+    const hundreths = time;
+
+    return `${addPrefix(minutes)}:${addPrefix(seconds)}.${addPrefix(
+      hundreths,
+    )}`;
+  };
+
+  const addPrefix = number => {
+    if (number < 10) {
+      return `0${number}`;
+    }
+    return String(number);
+  };
+
+  const start = () => {
+    // start upating time
+    setClockState('start');
+  };
+
+  const pause = () => {
+    // stop updating time
+    setClockState('pause');
+  };
+
+  const stop = () => {
+    setSecondsElapsed(0);
+    setClockState('new');
+  };
+
+  const handleShow = async () => {
+    await delay(3000);
+    setShow(true);
+  }
+
+  // const handleClose = () => {
+  //   resetGame();
+  //   setShow(false);
+  // }
+
+
+  const handleKeyboardInput = (e) => {
+    handleKeyPress(e.key,);
+  };
 
   const handleKeyPress = (key, e) => {
     const wordGridCopy = [...wordGrid];
@@ -46,12 +152,15 @@ function Game() {
         alert("That word is not allowed");
       else {
         if (newWord === targetWord) {
-          alert("YOU WIN!!");
+          handleShow();
           setGameState("win");
+          setFinalTime(secondsElapsed);
+          pause();
         }
         else if (currRow === 5) {
-          alert("YOU LOSE")
+          handleShow();
           setGameState("lose");
+          pause();
         }
         setCurrRow(currRow + 1);
         updateUsedLetters(newWord);
@@ -91,18 +200,58 @@ function Game() {
   });
 
   useEffect(() => {
-    const randomIdx = Math.floor(Math.random() * answers.length);
-    const randomTargetWord = answers[randomIdx];
-    setTargetWord(randomTargetWord);
+    resetTargetWord();
   }, [])
 
+  const getModalButtonText = (state) => {
+    if (gameState === "new") return "Start Game!";
+    if (gameState === "win") return "Play Again";
+    if (gameState === "lose") return "Try Again";
+  }
+
+  const getModalBodyText = (state) => {
+    if (gameState === "new") return "Press the Button to Start the Game and Timer"
+    if (gameState === "win") return "Great Job!";
+    if (gameState === "lose") return "Sorry, Better Luck Next Time";
+  }
+
   return (
-    <div className="container">
-      <div className="headerContainer">
-        <div className="resetButton" onClick={resetGame}>
-          RESET GAME
-        </div>
-      </div>
+    <div className="containerStyle">
+      <Modal show={show} centered>
+        <Modal.Header>
+          <Modal.Title>{getModalBodyText(gameState)}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{gameState === "win" && <div>
+          <Alert> Your Final Time: {formatTimeString(finalTime)}</Alert>
+          <h5>Top 10 Times:</h5>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Rank</th>
+                <th>Name</th>
+                <th>Time</th>
+              </tr>
+            </thead>
+            <tbody>
+              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((entry, index) => <tr>
+                <td>{entry}</td>
+                <td>Mark</td>
+                <td>1111 seconds</td>
+              </tr>)}
+            </tbody>
+          </Table>
+        </div>}
+          {gameState === "lose" && <img src="https://i.giphy.com/media/TJawtKM6OCKkvwCIqX/giphy.webp" alt="this slowpoke moves" style={{ width: "100%" }} />}
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={startTheGame}>
+            {getModalButtonText(gameState)}
+          </Button>
+        </Modal.Footer>
+      </Modal>
+
+      <div className="timer">{formatTimeString(secondsElapsed)}</div>
+
       <div className="gameBoardContainer">
         {
           wordGrid.map((word, index) =>
@@ -124,7 +273,7 @@ function Game() {
           usedLetters={usedLetters}
           targetWord={targetWord} />
       </div>
-    </div>
+    </div >
   );
 }
 
